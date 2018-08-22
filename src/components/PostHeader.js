42f7d51bc4601161f2fb51bd7f02446f8225126a
Brian Lichliter <brian.lichliter@mudaidesign.com>
2018-08-22 09:52:15 -0500

    tweaks

--- src/components/PostHeader.js
+++ src/components/PostHeader.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 class PostHeader extends React.Component {
     render() {
         return (
-            <div style={{minHeight: 200, height: "65vh", maxHeight: 400, backgroundColor: this.props.selectedProject.color, color: "white"}}>
+            <div style={{minHeight: 200, height: "50vh", maxHeight: 400, backgroundColor: this.props.selectedProject.color, color: "white"}}>
                 <Grid container spacing={0} justify="center" alignItems="center" style={{height: "calc(100% - 10px)"}}>
                     <img alt={this.props.selectedProject.image} style={{maxWidth: "90%", maxHeight: "90%", boxShadow: "rgba(0,0,0,.2) -5px 5px 50px 4px"}} src={this.props.selectedProject.imageURL}/>
                 </Grid>
